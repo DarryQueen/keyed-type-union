082c9254a6d4eef2a4e6feb10bfd35bd726a0274
Darren Zhao <darren@levro.com>
2023-04-25 12:01:28 -0400

    Prettier.

--- src/KeyedTypeUnion.ts
+++ src/KeyedTypeUnion.ts
@@ -1,19 +1,16 @@
-export type KeyedTypeUnion<
-  KeyedValues extends Record<string, unknown>,
-  TypeKey extends string = "type"
-> = {
-  [Type in keyof KeyedValues]: { [K in TypeKey]: Type } & {
-    [T in Type]: KeyedValues[Type];
-  };
+export type KeyedTypeUnion<KeyedValues extends Record<string, unknown>, TypeKey extends string = "type"> = {
+    [Type in keyof KeyedValues]: { [K in TypeKey]: Type } & {
+        [T in Type]: KeyedValues[Type];
+    };
 }[keyof KeyedValues];
 
 export type RestrictedTypeUnionForTypeKey<
-  TypeUnion extends KeyedTypeUnion<Record<string, unknown>, TypeKey>,
-  Type extends string,
-  TypeKey extends string
+    TypeUnion extends KeyedTypeUnion<Record<string, unknown>, TypeKey>,
+    Type extends string,
+    TypeKey extends string,
 > = TypeUnion extends Record<TypeKey, Type> ? TypeUnion : never;
 export type ValueForTypeKey<
-  TypeUnion extends KeyedTypeUnion<Record<string, unknown>, TypeKey>,
-  Type extends string,
-  TypeKey extends string
+    TypeUnion extends KeyedTypeUnion<Record<string, unknown>, TypeKey>,
+    Type extends string,
+    TypeKey extends string,
 > = TypeUnion extends Record<TypeKey, Type> ? TypeUnion[Type] : never;
