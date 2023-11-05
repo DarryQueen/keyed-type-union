305f6d2d5fd476631b73701c933e715668c1b025
Darren Zhao <slimfrog@gmail.com>
2023-11-05 11:49:36 -0500

    Use stronger typings for creators. (#8)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## Unreleased
 
+### Changed
+- Use stronger typings for creator function (`createTypeUnion`).
+
 ## 0.0.4 - 2023-04-25
 ### Changed
 - Third generic argument `TypeKey` is now optional and defaults to `"type"`.
--- src/__tests__/createTypeUnion.test.ts
+++ src/__tests__/createTypeUnion.test.ts
@@ -14,54 +14,103 @@ const ITypeUnion = createTypeUnion<ITypeUnion>({
     anotherTypeC: undefined,
 });
 
+type ITypeUnionWithTypeKey = KeyedTypeUnion<{ a: string; myTypeB: number }, "myType">;
+const ITypeUnionWithTypeKey = createTypeUnion<ITypeUnionWithTypeKey, "myType">(
+    {
+        a: undefined,
+        myTypeB: undefined,
+    },
+    "myType",
+);
+
 describe("typeUnionUtils", () => {
-    const typeUnionA = ITypeUnion.a("hello!");
-    const typeUnionB = ITypeUnion.typeB(35);
-    const typeUnionC = ITypeUnion.anotherTypeC({ foo: "bar", test: 1 });
+    describe("for default type key", () => {
+        const typeUnionA = ITypeUnion.a("hello!");
+        const typeUnionB = ITypeUnion.typeB(35);
+        const typeUnionC = ITypeUnion.anotherTypeC({ foo: "bar", test: 1 });
 
-    it("can create types", () => {
-        expect(typeUnionA).toEqual({ type: "a", a: "hello!" });
-        expect(typeUnionB).toEqual({ type: "typeB", typeB: 35 });
-        expect(typeUnionC).toEqual({ type: "anotherTypeC", anotherTypeC: { foo: "bar", test: 1 } });
+        it("can create types", () => {
+            expect(typeUnionA).toEqual({ type: "a", a: "hello!" });
+            expect(typeUnionB).toEqual({ type: "typeB", typeB: 35 });
+            expect(typeUnionC).toEqual({ type: "anotherTypeC", anotherTypeC: { foo: "bar", test: 1 } });
+        });
+
+        it("can check types", () => {
+            expect(ITypeUnion.isA(typeUnionA)).toBeTruthy();
+            expect(ITypeUnion.isTypeB(typeUnionA)).toBeFalsy();
+            expect(ITypeUnion.isAnotherTypeC(typeUnionA)).toBeFalsy();
+
+            expect(ITypeUnion.isA(typeUnionB)).toBeFalsy();
+            expect(ITypeUnion.isTypeB(typeUnionB)).toBeTruthy();
+            expect(ITypeUnion.isAnotherTypeC(typeUnionB)).toBeFalsy();
+
+            expect(ITypeUnion.isA(typeUnionC)).toBeFalsy();
+            expect(ITypeUnion.isTypeB(typeUnionC)).toBeFalsy();
+            expect(ITypeUnion.isAnotherTypeC(typeUnionC)).toBeTruthy();
+        });
+
+        it("can visit types", () => {
+            const visitA = jest.fn();
+            const visitB = jest.fn();
+            const visitC = jest.fn();
+
+            [typeUnionA, typeUnionB, typeUnionC].forEach((typeUnion) =>
+                ITypeUnion.visit(typeUnion, {
+                    a: (a) => {
+                        visitA();
+                        expect(a).toBe("hello!");
+                    },
+                    typeB: (typeB) => {
+                        visitB();
+                        expect(typeB).toBe(35);
+                    },
+                    anotherTypeC: ({ foo, test }) => {
+                        visitC();
+                        expect(foo).toBe("bar");
+                        expect(test).toBe(1);
+                    },
+                }),
+            );
+
+            [visitA, visitB, visitC].forEach((visit) => expect(visit).toBeCalledTimes(1));
+        });
     });
 
-    it("can check types", () => {
-        expect(ITypeUnion.isA(typeUnionA)).toBeTruthy();
-        expect(ITypeUnion.isTypeB(typeUnionA)).toBeFalsy();
-        expect(ITypeUnion.isAnotherTypeC(typeUnionA)).toBeFalsy();
+    describe("for custom type key", () => {
+        const typeUnionA = ITypeUnionWithTypeKey.a("hello!");
+        const typeUnionB = ITypeUnionWithTypeKey.myTypeB(35);
 
-        expect(ITypeUnion.isA(typeUnionB)).toBeFalsy();
-        expect(ITypeUnion.isTypeB(typeUnionB)).toBeTruthy();
-        expect(ITypeUnion.isAnotherTypeC(typeUnionB)).toBeFalsy();
+        it("can create types", () => {
+            expect(typeUnionA).toEqual({ myType: "a", a: "hello!" });
+            expect(typeUnionB).toEqual({ myType: "myTypeB", myTypeB: 35 });
+        });
 
-        expect(ITypeUnion.isA(typeUnionC)).toBeFalsy();
-        expect(ITypeUnion.isTypeB(typeUnionC)).toBeFalsy();
-        expect(ITypeUnion.isAnotherTypeC(typeUnionC)).toBeTruthy();
-    });
+        it("can check types", () => {
+            expect(ITypeUnionWithTypeKey.isA(typeUnionA)).toBeTruthy();
+            expect(ITypeUnionWithTypeKey.isMyTypeB(typeUnionA)).toBeFalsy();
 
-    it("can visit types", () => {
-        const visitA = jest.fn();
-        const visitB = jest.fn();
-        const visitC = jest.fn();
+            expect(ITypeUnionWithTypeKey.isA(typeUnionB)).toBeFalsy();
+            expect(ITypeUnionWithTypeKey.isMyTypeB(typeUnionB)).toBeTruthy();
+        });
 
-        [typeUnionA, typeUnionB, typeUnionC].forEach((typeUnion) =>
-            ITypeUnion.visit(typeUnion, {
-                a: (a) => {
-                    visitA();
-                    expect(a).toBe("hello!");
-                },
-                typeB: (typeB) => {
-                    visitB();
-                    expect(typeB).toBe(35);
-                },
-                anotherTypeC: ({ foo, test }) => {
-                    visitC();
-                    expect(foo).toBe("bar");
-                    expect(test).toBe(1);
-                },
-            }),
-        );
+        it("can visit types", () => {
+            const visitA = jest.fn();
+            const visitB = jest.fn();
 
-        [visitA, visitB, visitC].forEach((visit) => expect(visit).toBeCalledTimes(1));
+            [typeUnionA, typeUnionB].forEach((typeUnion) =>
+                ITypeUnionWithTypeKey.visit(typeUnion, {
+                    a: (a) => {
+                        visitA();
+                        expect(a).toBe("hello!");
+                    },
+                    myTypeB: (myTypeB) => {
+                        visitB();
+                        expect(myTypeB).toBe(35);
+                    },
+                }),
+            );
+
+            [visitA, visitB].forEach((visit) => expect(visit).toBeCalledTimes(1));
+        });
     });
 });
